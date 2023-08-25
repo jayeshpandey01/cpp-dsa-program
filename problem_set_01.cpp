65170c6d3ea37dd7477127d86562cfc34b8ea80b
Jayesh Pandey <116277383+jayeshpandey01@users.noreply.github.com>
2023-08-25 19:59:40 +0530

    Add files via upload
    it content problem set question with answers.

---
+++ problem_set_01.cpp
@@ -0,0 +1,39 @@
+// swapping two number
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int num1, num2, num3;
+//     cout<<"Enter first number: ";cin>>num1;
+//     cout<<"Enter second number: ";cin>>num2;
+//     // swapping two number 
+//     num3 = num1;
+//     num1 = num2;
+//     num2 = num3;
+//     cout<<"After swapping, first number will be: "<<num1<<endl;
+//     cout<<"After swapping, second number will be: "<<num2<<endl;
+// }
+
+// area of a rectangle
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int length, breath, area;
+//     cout<<"Enter length of a rectangle: ";cin>>length;
+//     cout<<"Enter breath of a rectangle: "; cin>>breath;
+//     // write formula of a rectangle 
+//     area = length * breath;
+//     cout<<"Area of a rectangle is: "<<area<<endl;
+// }
+
+// area of a triangle 
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int base, height, area;
+//     cout<<"Enter base of a triangle: ";cin>>base;
+//     cout<<"Enter height of a triangle: "; cin>>height;
+//     // write formula of a triangle
+//     area = 0.5 * base * height;
+//     cout<<"Area of a triangle is: "<<area<<endl;
+// }
+
